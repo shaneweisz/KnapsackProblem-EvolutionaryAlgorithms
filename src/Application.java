c6b197b5e58bcf70ef0a9c8ca61cee569e541c37
Shane Weisz <shaneweisz@gmail.com>
2020-03-24 08:16:58 +0200

    Split run into runGA, runSA, runPSO

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 /**
  * Main driver for the application. Either run the solver on a specific
@@ -10,75 +11,117 @@ public class Application {
     private final static String GA_PATH = "/Users/shaneweisz/Documents/UCT/Honours/Evolutionary Computing/Assignment/configurations/json_configuration_ga_default/";
     private final static String SA_PATH = "/Users/shaneweisz/Documents/UCT/Honours/Evolutionary Computing/Assignment/configurations/json_configuration_sa_default/";
     private final static String PSO_PATH = "/Users/shaneweisz/Documents/UCT/Honours/Evolutionary Computing/Assignment/configurations/json_configuration_pso_default/";
+    private final static int NUM_GA_CONFIGS = 28;
+    private final static int NUM_SA_CONFIGS = 25;
+    private final static int NUM_PSO_CONFIGS = 25;
 
     public static void main(String[] args) {
-        if (args[0].equals("-configuration")) {
+        if (args[0].equalsIgnoreCase("-configuration")) {
             // Extract the desired configuration to run
             // Options include: ga_default_01, ..., ga_best, etc.
             String configuration = args[1];
-            String algo = configuration.substring(0, configuration.indexOf("_"));
-            try {
-                if (algo.equals("ga")) { // Then it is a Genetic Algorithm configuration
-                    String fileName = GA_PATH + configuration + ".json";
-                    Scanner scFile = new Scanner(new File(fileName));
-                    scFile.useDelimiter(",");
-
-                    String selectionMethod = getJSONValueFromLine(scFile.next());
-                    scFile.next(); // Skip the configuration since we already know it
-                    double mutationRatio = Double.parseDouble(getJSONValueFromLine(scFile.next()));
-                    double crossoverRatio = Double.parseDouble(getJSONValueFromLine(scFile.next()));
-                    String crossoverMethod = getJSONValueFromLine(scFile.next());
-                    String mutationMethod = getJSONValueFromLine(scFile.next());
-                    GeneticAlgorithm.run(configuration, selectionMethod, crossoverMethod, crossoverRatio,
-                            mutationMethod, mutationRatio);
-
-                    scFile.close();
-                } else if (algo.equals("sa")) {
-                    String fileName = SA_PATH + configuration + ".json";
-                    Scanner scFile = new Scanner(new File(fileName));
-                    scFile.useDelimiter(",");
-
-                    int initialTemperature = Integer.parseInt(getJSONValueFromLine(scFile.next()));
-                    scFile.next(); // Skip the configuration since we already know it
-                    double coolingRate = Double.parseDouble(getJSONValueFromLine(scFile.next()));
-                    System.out.println(initialTemperature);
-                    System.out.println(coolingRate);
-                    // SimulatedAnnealing.run(configuration, initialTemperature, coolingRate);
-                } else if (algo.equals("pso")) {
-                    String fileName = PSO_PATH + configuration + ".json";
-                    Scanner scFile = new Scanner(new File(fileName));
-                    scFile.useDelimiter(",");
-
-                    int minVelocity = Integer.parseInt(getJSONValueFromLine(scFile.next()));
-                    int maxVelocity = Integer.parseInt(getJSONValueFromLine(scFile.next()));
-                    double inertia = Double.parseDouble(getJSONValueFromLine(scFile.next()));
-                    scFile.next(); // Skip the configuration since we already know it
-                    int numParticles = Integer.parseInt(getJSONValueFromLine(scFile.next()));
-                    double c1 = Double.parseDouble(getJSONValueFromLine(scFile.next()));
-                    double c2 = Double.parseDouble(getJSONValueFromLine(scFile.next()));
-                    System.out.println(minVelocity);
-                    System.out.println(maxVelocity);
-                    System.out.println(inertia);
-                    System.out.println(numParticles);
-                    System.out.println(c1);
-                    System.out.println(c2);
-                    // ParticleSwarmOptimization.run(configuration, numParticles, minVelocity,
-                    // maxVelocity, c1, c2, inertia);
-                } else {
-                    // The algorithm must be one of the above 3 options so will not get here
-                }
-
-            } catch (FileNotFoundException e) {
-                System.out.println(e);
-            }
+            runAlgorithm(configuration);
 
         } else if (args[0].equals("-search_best_configuration")) {
             // Get the method to find the best configuration for
             // Options: ga, sa and pso
             String method = args[1];
-            System.out.println(method);
+            DecimalFormat decimalFormat = new DecimalFormat("00");
+            if (method.equalsIgnoreCase("ga")) {
+                for (int i = 1; i <= NUM_GA_CONFIGS; i++) {
+                    String configuration = "ga_default_" + decimalFormat.format(i);
+                    runGA(configuration);
+                }
+            } else if (method.equalsIgnoreCase("sa")) {
+                for (int i = 1; i <= NUM_SA_CONFIGS; i++) {
+                    String configuration = "sa_default_" + decimalFormat.format(i);
+                    runSA(configuration);
+                }
+            } else if (method.equalsIgnoreCase("pso")) {
+                for (int i = 1; i <= NUM_PSO_CONFIGS; i++) {
+                    String configuration = "pso_default_" + decimalFormat.format(i);
+                    runPSO(configuration);
+                }
+            }
         }
 
+    }
+
+    private static void runAlgorithm(String configuration) {
+        String algo = configuration.substring(0, configuration.indexOf("_"));
+        if (algo.equalsIgnoreCase("ga")) {
+            runGA(configuration);
+        } else if (algo.equalsIgnoreCase("sa")) {
+            runSA(configuration);
+        } else if (algo.equalsIgnoreCase("pso")) {
+            runPSO(configuration);
+        }
+    }
+
+    private static void runGA(String configuration) {
+        try {
+            String fileName = GA_PATH + configuration + ".json";
+            Scanner scFile = new Scanner(new File(fileName));
+            scFile.useDelimiter(",");
+
+            String selectionMethod = getJSONValueFromLine(scFile.next());
+            scFile.next(); // Skip the configuration since we already know it
+            double mutationRatio = Double.parseDouble(getJSONValueFromLine(scFile.next()));
+            double crossoverRatio = Double.parseDouble(getJSONValueFromLine(scFile.next()));
+            String crossoverMethod = getJSONValueFromLine(scFile.next());
+            String mutationMethod = getJSONValueFromLine(scFile.next());
+            GeneticAlgorithm.run(configuration, selectionMethod, crossoverMethod, crossoverRatio, mutationMethod,
+                    mutationRatio);
+
+            scFile.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+    }
+
+    private static void runSA(String configuration) {
+        try {
+            String fileName = SA_PATH + configuration + ".json";
+            Scanner scFile = new Scanner(new File(fileName));
+            scFile.useDelimiter(",");
+
+            int initialTemperature = Integer.parseInt(getJSONValueFromLine(scFile.next()));
+            scFile.next(); // Skip the configuration since we already know it
+            double coolingRate = Double.parseDouble(getJSONValueFromLine(scFile.next()));
+            System.out.println(initialTemperature);
+            System.out.println(coolingRate);
+            // SimulatedAnnealing.run(configuration, initialTemperature, coolingRate);
+
+            scFile.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+    }
+
+    private static void runPSO(String configuration) {
+        try {
+            String fileName = PSO_PATH + configuration + ".json";
+            Scanner scFile = new Scanner(new File(fileName));
+            scFile.useDelimiter(",");
+
+            int minVelocity = Integer.parseInt(getJSONValueFromLine(scFile.next()));
+            int maxVelocity = Integer.parseInt(getJSONValueFromLine(scFile.next()));
+            double inertia = Double.parseDouble(getJSONValueFromLine(scFile.next()));
+            scFile.next(); // Skip the configuration since we already know it
+            int numParticles = Integer.parseInt(getJSONValueFromLine(scFile.next()));
+            double c1 = Double.parseDouble(getJSONValueFromLine(scFile.next()));
+            double c2 = Double.parseDouble(getJSONValueFromLine(scFile.next()));
+            System.out.println(minVelocity);
+            System.out.println(maxVelocity);
+            System.out.println(inertia);
+            System.out.println(numParticles);
+            System.out.println(c1);
+            System.out.println(c2);
+            // ParticleSwarmOptimization.run(configuration, numParticles, minVelocity,
+            // maxVelocity, c1, c2, inertia);
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
     }
 
     /** Takes in a line in the format "[key]":"[value]", and returns [value] */
--- src/GeneticAlgorithm.java
+++ src/GeneticAlgorithm.java
@@ -1,5 +1,5 @@
 public class GeneticAlgorithm {
-    private final static int POPULATION_SIZE = 1000;
+    private final static int POPULATION_SIZE = 250;
     private final static double ELITISM_RATIO = 0.1;
 
     public static void run(String configuration, String selectionMethod, String crossoverMethod, double crossoverRatio,
@@ -20,17 +20,21 @@ public class GeneticAlgorithm {
             bestChromosome = population.getPopulation()[0];
             if (bestChromosome.getFitness() > currentBestFitness) {
                 currentBestFitness = bestChromosome.getFitness();
-                System.out.println("Generation " + ProblemConfiguration.instance.decimalFormat.format(generation)
-                        + ": Weight = " + bestChromosome.getTotalWeight() + ", Value = " + bestChromosome.getFitness()
-                        + ", Knapsack = " + bestChromosome);
+                // System.out.println("Generation " +
+                // ProblemConfiguration.instance.decimalFormat.format(generation)
+                // + ": Weight = " + bestChromosome.getTotalWeight() + ", Value = " +
+                // bestChromosome.getFitness()
+                // + ", Knapsack = " + bestChromosome);
             }
         }
         generation--; // We stopped before evolving the next generation
 
         // Output final best solution
-        System.out.println("Generation : " + ProblemConfiguration.instance.decimalFormat.format(generation) + " : "
+        System.out.println("Generation " + ProblemConfiguration.instance.decimalFormat.format(generation) + " : "
                 + bestChromosome.getTotalValue());
-        System.out.println("numberOfCrossoverOperations : " + population.getNumberOfCrossoverOperations());
-        System.out.println("numberOfMutationOperations  : " + population.getNumberOfMutationOperations());
+        // System.out.println("numberOfCrossoverOperations : " +
+        // population.getNumberOfCrossoverOperations());
+        // System.out.println("numberOfMutationOperations : " +
+        // population.getNumberOfMutationOperations());
     }
 }
